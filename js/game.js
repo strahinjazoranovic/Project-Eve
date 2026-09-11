b163256084c28e6d648110a07418aa1a8cdd74a8
strahinjazoranovic <strahinja.zoranovic2@gmail.com>
2026-09-11 14:34:54 +0200

    V1.0.3.3
    1. Increased beatGameScore from 16000 to 160000, as this was the intended score that was needed to beat the game from V1 but was incorrectly changed for testing purposes

--- js/game.js
+++ js/game.js
@@ -634,7 +634,7 @@ let game = {
 };
 
 // Score needed to beat the game
-const beatGameScore = 16000;
+const beatGameScore = 160000;
 
 // Invader spawn interval using difficulty
 var invaderSpawnInterval = Math.floor(
